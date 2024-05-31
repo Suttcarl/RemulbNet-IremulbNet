b2c1d0b57bc5d778998e9966ce23d391e18dd283
Suttcarl <157381765+Suttcarl@users.noreply.github.com>
2024-05-31 10:20:23 +0800

    Add files via upload

---
+++ nimi-imagenet/Iremulbnet_train.py
@@ -0,0 +1,172 @@
+import os
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import transforms, datasets
+from tqdm import tqdm
+from Iremulbnet import iremulbnet
+import matplotlib.pyplot as plt
+import time
+from PIL import Image
+import re
+from thop import profile
+from thop import clever_format
+from my_dataset import MyDataSet
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.cuda.synchronize()
+    startg = time.time()
+    print("using {} device.".format(device))
+
+    data_transform = {
+        "train": transforms.Compose([transforms.Resize(256),
+                                     transforms.RandomResizedCrop(224),
+                                     transforms.RandomHorizontalFlip(),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
+        "val": transforms.Compose([transforms.Resize(256),
+                                   #transforms.Resize(256),
+                                   transforms.CenterCrop(224),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
+
+
+    json_path = "./classes_name.json"
+    image_path = os.path.join("/home/sutiantian/miniimagenet/data/")  # flower data set path
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    train_dataset = MyDataSet(root_dir=image_path,
+                              csv_name="new_train.csv",
+                              json_path=json_path,
+                              transform=data_transform["train"])
+    train_num = len(train_dataset)
+
+
+    batch_size = 128
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    print('Using {} dataloader workers every process'.format(nw))
+
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size, shuffle=True,
+                                               num_workers=nw)
+
+    val_dataset = MyDataSet(root_dir=image_path,
+                            csv_name="new_val.csv",
+                            json_path=json_path,
+                            transform=data_transform["val"])
+    val_num = len(val_dataset)
+    validate_loader = torch.utils.data.DataLoader(val_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+
+    print("using {} images for training, {} images for validation.".format(train_num,
+                                                                           val_num))
+    net = iremulbnet(num_classes=100).to(device)
+
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    '''params = [p for p in net.parameters() if p.requires_grad]
+    optimizer = optim.Adam(params, lr=0.0001)'''
+    
+    
+    
+    epochs = 200
+    best_acc = 0.0
+    test_acc=0.0
+    a=0
+    save_path = './iremulbnet.pth'
+    train_steps = len(train_loader)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    plt.title("training loss and val_accurate")
+    ax.set_xlabel("epoch")
+    ax.set_ylabel("")
+    y1=[]#loss
+    y2=[]#val
+    x=[]#epoch
+
+
+    for epoch in range(epochs):
+        # train
+        net.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader)
+        for step, data in enumerate(train_bar):
+            images, labels = data
+            optimizer.zero_grad()
+            logits = net(images.to(device))
+            loss = loss_function(logits, labels.to(device))
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+
+            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
+                                                                     epochs,
+                                                                     loss)
+
+        # validate
+        net.eval()
+        acc = 0.0  # accumulate accurate number / epoch
+        with torch.no_grad():
+            val_bar = tqdm(validate_loader)
+            for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = net(val_images.to(device))
+                # loss = loss_function(outputs, test_labels)
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+
+                val_bar.desc = "valid epoch[{}/{}]".format(epoch + 1,
+                                                           epochs)
+
+
+        val_accurate = acc / val_num
+        y1.append(running_loss / train_steps)
+        y2.append(val_accurate)
+        x.append(epoch+1)
+        print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f  ' %
+              (epoch + 1, running_loss / train_steps, val_accurate))
+
+        if val_accurate > best_acc:
+            best_acc = val_accurate
+            torch.save(net.state_dict(), save_path)
+            a=epoch
+
+    
+            
+    line1, = ax.plot(x, y1, 'b-', label='training loss')
+    line2, = ax.plot(x, y2, 'r-', label='val_accurate')
+    print('epoch:  %d   val_acc:  %.3f   '%(a+1,y2[a]))
+    torch.cuda.synchronize()
+    endg = time.time()
+    print('训练时间: %.3f ' % (endg-startg))
+    
+    fil=open("iremulbnet.txt","w")
+    y=[float('{:.3f}'.format(i)) for i in y1]
+    fil.writelines(str(y)+str('\r'))
+    y=[float('{:.3f}'.format(i)) for i in y2]
+    fil.writelines(str(y)+str('\r'))
+    fil.close()
+    
+    '''total = sum([param.nelement() for param in net.parameters()])
+    print("Number of parameter: %.2fM" % (total/1e6))'''
+    model = iremulbnet()
+    input = torch.randn(1,3,224,224)
+    flops,params = profile(model,inputs=(input, ))
+    flops,params = clever_format([flops,params],"%.3f")
+    print("FLOPS: %s PARAMS: %s  "%(flops,params))
+    os.system("python batch_predict.py")
+    print('Finished Training')
+    ax.legend(handles=[line1, line2], labels=['training loss', 'val_accurate'], loc='best')
+    plt.savefig('clossandacc.jpg')
+    plt.pause(3)
+    plt.close(fig)
+    
+if __name__ == '__main__':
+    main()
---
+++ nimi-imagenet/batch_predict.py
@@ -0,0 +1,92 @@
+import os
+import json
+import re
+import torch
+from PIL import Image
+from torchvision import transforms, datasets
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from my_dataset import MyDataSet
+from thop import profile
+from thop import clever_format
+
+
+from iresnet2 import resnet50  
+from dmodel import densenet121  
+from model_v2 import MobileNetV2 
+from model_v3 import mobilenet_v3_large  
+from smodel import shufflenet_v2_x1_0   
+from rgmodel import create_regnet  
+from PeleeNet import PeleeNet 
+from inceptionnext import inceptionnext_tiny
+from Iremulbnet import iremulbnet as model0
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [#transforms.Resize([380,300]),
+         transforms.Resize(256),
+         transforms.CenterCrop(224),
+         transforms.ToTensor(),
+         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    # load image
+    #cwd = os.getcwd()
+    json_path = "./classes_name.json"
+    image_path = os.path.join("/home/sutiantian/miniimagenet/data/")  # flower data set path
+    assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
+    
+    
+    batch_size = 32
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
+    val_dataset = MyDataSet(root_dir=image_path,
+                            csv_name="new_val.csv",
+                            json_path=json_path,
+                            transform=data_transform)
+    val_num = len(val_dataset)
+    validate_loader = torch.utils.data.DataLoader(val_dataset,
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
+
+    print(val_num)
+
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    json_file = open(json_path, "r")
+    class_indict = json.load(json_file)
+
+    # create model
+    model = model0(num_classes=100).to(device)
+    weights_path = "./iremulbnet.pth"
+    assert os.path.exists(weights_path), "file: '{}' dose not exist.".format(weights_path)
+    model.load_state_dict(torch.load(weights_path, map_location=device))
+  
+    tn=0
+    fn=0
+    
+    acc = 0.0 
+    # prediction
+    model.eval()
+    with torch.no_grad():
+        # predict class
+        val_bar = tqdm(validate_loader)
+        for val_data in val_bar:
+                val_images, val_labels = val_data
+                outputs = model(val_images.to(device))
+                # loss = loss_function(outputs, test_labels)
+                predict_y = torch.max(outputs, dim=1)[1]
+                acc += torch.eq(predict_y, val_labels.to(device)).sum().item()
+        val_accurate = acc / val_num
+    print(val_accurate)
+    
+    model = model0()
+    input = torch.randn(1,3,224,224)
+    flops,params = profile(model,inputs=(input, ))
+    flops,params = clever_format([flops,params],"%.3f")
+    print("FLOPS: %s PARAMS: %s  "%(flops,params))
+if __name__ == '__main__':
+    main()
+    
